90d1ef53376b7e30734025ad4221fddca6b8e8db
Jeremy Razafinirina <jeremy@MacBook-Pro-de-Jeremy.local>
2018-04-13 18:41:48 +0200

    fix(slider)disable slider on desktop

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,43 +1,50 @@
-const test = document.querySelector('.support-card.active');
 const supportCardList = document.querySelectorAll('.support-card');
 const length = supportCardList.length;
 const supportCard = document.querySelector('.s-support__inner');
 let sliderIndex = 1;
+let windowWidth = 0;
 
-console.log(sliderIndex);
+
 
 function defineStart(e) {
 	this.setAttribute('data-start', e.changedTouches[0].clientX)
 }
 
 function goSlide(e) {
-	console.log('length :' + length);
-	console.log('slider :' + sliderIndex);
-	supportCardList[sliderIndex].classList.remove("active");
-	if (this.getAttribute('data-start') > e.changedTouches[0].clientX) {
-		if (sliderIndex < length) {
-			if (sliderIndex === 1) {
-				supportCard.style.transform = 'translateX(-50%)';
-			} else {
-				supportCard.style.transform = 'translateX(-10px)';
+	if (windowWidth < 992) {
+		supportCardList[sliderIndex].classList.remove("active");
+		if (this.getAttribute('data-start') > e.changedTouches[0].clientX) {
+			if (sliderIndex < length) {
+				if (sliderIndex === 1) {
+					supportCard.style.transform = 'translateX(-50%)';
+				} else {
+					supportCard.style.transform = 'translateX(-10px)';
+				}
+				sliderIndex++;
 			}
-
-			sliderIndex++;
-		}
-	} else if (this.getAttribute('data-start') < e.changedTouches[0].clientX) {
-		if (sliderIndex > 0) {
-			if (sliderIndex === 1) {
-				supportCard.style.transform = 'translateX(50%)';
-			} else {
-				supportCard.style.transform = 'translateX(10px)';
+		} else if (this.getAttribute('data-start') < e.changedTouches[0].clientX) {
+			if (sliderIndex > 0) {
+				if (sliderIndex === 1) {
+					supportCard.style.transform = 'translateX(50%)';
+				} else {
+					supportCard.style.transform = 'translateX(10px)';
+				}
+				sliderIndex--;
 			}
-			sliderIndex--;
 		}
+		supportCardList[sliderIndex].classList.add("active");
 	}
-	supportCardList[sliderIndex].classList.add("active");
 }
 
 supportCardList.forEach(function (el) {
 	el.addEventListener('touchstart', defineStart);
 	el.addEventListener('touchend', goSlide)
+});
+
+window.addEventListener('resize', () => {
+	windowWidth = window.innerWidth;
+
+	if (windowWidth > 992) {
+		supportCard.style.transform = 'translateX(0)';
+	}
 });
